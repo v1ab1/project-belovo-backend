4611f4b1e09d89a859414a74153c852a39ad31be
v1ab1 <sharkandmouse@gmail.com>
2022-12-06 03:41:24 +0500

    big work

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 import express from 'express';
+import ImageDataURI from 'image-data-uri';
+import cors from "cors";
 
 const app = express();
 
+app.use(express.json());
+
+app.use(cors());
+
 app.get('/', (req, res) => {
-    console.log('hello');
+    res.send('Server is fine');
+});
+
+app.post("/send", (req, res) => {
+    const name = (Math.random() + 1).toString(36).substring(7);
+    ImageDataURI.outputFile(req.body.dataURL, `./media/${name}.png`);
+    res.json({
+        "success": true
+    });
 });
 
 app.listen(4000, (err) => {
